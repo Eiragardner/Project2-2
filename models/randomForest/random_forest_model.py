610f55fa312e236461747b1ebc5198948c96323e
= <t.koc@student.maastrichtuniversity.nl>
2025-04-15 23:36:41 +0200

    random forest plots updated

--- models/randomForest/random_forest_model.py
+++ models/randomForest/random_forest_model.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
+import textwrap
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("prepared_data.csv")
+df = pd.read_csv("C:/Users/tahai/PycharmProjects/Project2-2/prepared_data.csv")
 
 X = df.iloc[:, [0] + list(range(2, df.shape[1]))]  
 y = df.iloc[:, 1] 
@@ -36,10 +37,43 @@ plt.ylabel("Predicted prices")
 plt.title("Actual vs Predicted prices")
 plt.grid(True)
 plt.tight_layout()
-#plt.show()
+plt.show()
+
+# Feature Importance Plot
+importances = model.feature_importances_
+features = X.columns
+
+N = 20  # Adjust based on your needs
+sorted_idx = np.argsort(importances)[-N:]  # Top N indices
+importances_top = importances[sorted_idx]
+features_top = features[sorted_idx]
+
+plt.figure(figsize=(10, 6))
+plt.barh(range(len(sorted_idx)), importances[sorted_idx], align='center', height=0.8)
+plt.yticks(range(len(sorted_idx)), features[sorted_idx], fontsize =10, ha='right')
+plt.subplots_adjust(left=0.3)
+plt.xlabel("Importance")
+plt.title(f"Top {N} Feature Importance")
+plt.grid(axis='x', alpha=0.5)
+plt.show()
+
+# Residual vs Predicted Values Plot
+
+residuals = y_test - y_pred
+
+plt.figure(figsize=(8, 6))
+plt.scatter(y_pred, residuals, alpha=0.5)
+plt.axhline(y=0, color='r', linestyle='--')
+plt.xlabel("Predicted Prices")
+plt.ylabel("Residuals")
+plt.title("Residuals vs Predicted Prices")
+plt.grid(True)
+plt.tight_layout()
+plt.show()
+
 
 try:
-    user_data = pd.read_csv("to_predict.csv")
+    user_data = pd.read_csv("C:/Users/tahai/PycharmProjects/Project2-2/to_predict.csv")
     user_data = user_data.iloc[:, [0] + list(range(2, user_data.shape[1]))]
 
     predicted_prices = model.predict(user_data)
